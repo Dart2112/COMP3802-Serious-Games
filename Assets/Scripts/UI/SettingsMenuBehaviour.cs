4223d20617fcbac302e03d59eb3a57c425ca1a58
ches0072 <ches0072@flinders.edu.au>
2021-09-01 15:47:27 +0930

    Fixed UI issue
    The UI issue was due to not having graphic raycaster on sub canvas component

--- Assets/Scripts/UI/SettingsMenuBehaviour.cs
+++ Assets/Scripts/UI/SettingsMenuBehaviour.cs
@@ -1,55 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenuBehaviour : MonoBehaviour
 {
     public GameObject testObj;
     public GameObject UICanvas;
+    public GameObject masterVolume;
+    public GameObject brightness;
+    public GameObject qualityLevelLabel;
+    private Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Starting");
+        SetVolume();
+        UpdateQualityLabel();
     }
 
-    public void SetVolume(float value)
+    /* --- Volume Settings ---  */
+    public void SetVolume()
     {
-        AudioListener.volume = value;
+        slider = masterVolume.GetComponent<UnityEngine.UI.Slider>();
+        AudioListener.volume = slider.value;
         UpdateVolumeLabel();
     }
 
     public void UpdateVolumeLabel()
     {
-        var masterVolume = UICanvas.transform.Find("MasterVolume");
-        masterVolume.GetComponent<UnityEngine.UI.Text>().text = "Master Volume - " + (AudioListener.volume * 100).ToString("f2") + "%";
+        Debug.Log("Updating Volume " + AudioListener.volume*100);
+        var masterVolumeLabel = masterVolume.transform.Find("MasterVolumeLabel");
+        masterVolumeLabel.GetComponent<UnityEngine.UI.Text>().text = "Master Volume - " + (AudioListener.volume * 100).ToString("f2") + "%";
     }
 
-    public void IncreaseQuality() {
+    /* --- Quality Settings ---  */
+    public void IncreaseQuality() 
+    {
         QualitySettings.IncreaseLevel();
         UpdateQualityLabel();
     }
 
-    public void DecreaseQuality() {
-        QualitySettings.DecreaseLevel();
-        
+    public void DecreaseQuality() 
+    {
+        QualitySettings.DecreaseLevel();        
         UpdateQualityLabel();
     }
 
-    public void UpdateQualityLabel() {
+    public void UpdateQualityLabel() 
+    {
         int currentQuality = QualitySettings.GetQualityLevel();
         string qualityName = QualitySettings.names[currentQuality];
 
-        Debug.Log("Current quality is " + qualityName);
+        // Updating the label
+        qualityLevelLabel.GetComponent<UnityEngine.UI.Text>().text = "Quality Level - " + qualityName;
+    }
 
-        // Setting the display output
-        var qualityLevel = UICanvas.transform.Find("QualityLevelLabel");
-        qualityLevel.GetComponent<UnityEngine.UI.Text>().text = "Quality Level - " + qualityName;
+    /* --- Brightness Settings ---  */
+    // Maybe use screen overlay?
+    public void SetBrightness() 
+    { 
+
+    }
+
+    public void UpdateBrightness() { 
+    
     }
 
     public void testOff()
     {
-        Debug.Log("reverse setting");
+        //Debug.Log("reverse setting");
         testObj.SetActive(!testObj.activeSelf);
     }
 }
