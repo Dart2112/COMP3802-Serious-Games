795029cb4d0a40caae158972a54f98dffeaca73b
ches0072 <ches0072@flinders.edu.au>
2021-09-09 12:20:20 +0930

    Greeting Screen and main menu
    Added a greet screen and main menu, linked the UI elements together using a C# script.

---
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public GameObject greetScreen;
+    public GameObject mainMenu;
+    public GameObject playScene;
+    public GameObject noteBook;
+    public GameObject settings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        greetScreen.SetActive(true);
+        mainMenu.SetActive(false);
+        settings.SetActive(false);
+    }
+
+
+    public void OpenMainMenu() {
+        greetScreen.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void CloseMainMenu() {
+        mainMenu.SetActive(false);
+        greetScreen.SetActive(true);
+    }
+    
+    public void OpenSettingsMenu() {
+        mainMenu.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void CloseSettingsMenu() {
+        settings.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
