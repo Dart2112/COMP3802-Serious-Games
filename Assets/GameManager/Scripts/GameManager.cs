ffc9c38dd4df6cb803483f12a84dda4d3d1546bb
Benjamin <DNAblue2112@gmail.com>
2021-10-28 00:37:35 +1030

    Updated GameManager
    Added new method to revert to another scene that is still loaded. This should be used when closing mini-games

--- Assets/GameManager/Scripts/GameManager.cs
+++ Assets/GameManager/Scripts/GameManager.cs
@@ -8,46 +8,57 @@ namespace GameManager.Scripts
     {
         //The first scene that needs to be loaded for the game
         public string mainSceneName;
+
         //The scene being loaded (This is given by other classes)
         private string _sceneName;
+
         //The scene that should be active (Might still be loading though)
         private string _activeScene = "";
+
         //List of scenes that need to be unloaded (Probably doesnt need to be a list but its just safer to do this)
         private readonly List<string> _unload = new List<string>();
 
         private void Start()
         {
             //Load the main scene and dont unload anything
-            PlayerPrefs.SetString("SceneToLoad", mainSceneName);
-            PlayerPrefs.SetString("UnloadScene", "");
+            PlayerPrefs.SetString("GameManager.LoadScene", mainSceneName);
+            PlayerPrefs.SetString("GameManager.UnloadScene", "");
+            PlayerPrefs.SetString("GameManager.ActiveScene", mainSceneName);
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
             //Fetch this value to check if a scene needs to be loaded
-            _sceneName = PlayerPrefs.GetString("SceneToLoad");
+            _sceneName = PlayerPrefs.GetString("GameManager.LoadScene");
             //Check if there is one to load
             if (_sceneName != "")
             {
-                //Get the name of the scene we need to unload
-                string unloadScene = PlayerPrefs.GetString("UnloadScene");
-                //Clear these ASAP to ensure that this doesnt get called again
-                PlayerPrefs.SetString("SceneToLoad", "");
-                PlayerPrefs.SetString("UnloadScene", "");
+                //Clear the variable to only load the scene once
+                PlayerPrefs.SetString("GameManager.LoadScene", "");
                 //Load the scene
                 SceneManager.LoadScene(_sceneName, LoadSceneMode.Additive);
-                //Mark it as the scene that should be active
-                _activeScene = _sceneName;
-                //If we are unloading a scene then we add it to the list here
-                if (unloadScene != "")
-                {
-                    _unload.Add(unloadScene);
-                }
+            }
+
+            string unloadScene = PlayerPrefs.GetString("GameManager.UnloadScene");
+            //If we are unloading a scene then we add it to the list here
+            if (unloadScene != "")
+            {
+                _unload.Add(unloadScene);
+                PlayerPrefs.SetString("GameManager.UnloadScene", "");
+            }
+
+            string activeScene = PlayerPrefs.GetString("GameManager.ActiveScene");
+            //Set the active scene variable
+            if (activeScene != "")
+            {
+                _activeScene = activeScene;
+                PlayerPrefs.SetString("GameManager.ActiveScene", "");
             }
 
             //Check if the correct scene is active
             bool setActive = SceneManager.GetActiveScene().name != _activeScene;
+            
             //Loop over all the loaded or loading scenes
             for (int i = 0; i < SceneManager.sceneCount; i++)
             {
@@ -57,6 +68,7 @@ namespace GameManager.Scripts
                 {
                     SceneManager.UnloadSceneAsync(s);
                 }
+
                 //If this is the scene that should be active and its ready then we set it as the active scene
                 if (setActive && s.name == _activeScene && s.isLoaded)
                 {
@@ -65,11 +77,18 @@ namespace GameManager.Scripts
             }
         }
 
+        //Use this to unload a mini-game and revert back to another scene
+        public static void UnloadScene(string sceneToUnload, string newActiveScene)
+        {
+            PlayerPrefs.SetString("GameManager.UnloadScene", sceneToUnload);
+            PlayerPrefs.SetString("GameManager.ActiveScene", newActiveScene);
+        }
+
         //Use this to load a Scene, Leave unload scene blank if you dont want to unload a scene
         public static void LoadNewScene(string sceneToLoad, string unloadScene)
         {
-            PlayerPrefs.SetString("SceneToLoad", sceneToLoad);
-            PlayerPrefs.SetString("UnloadScene", unloadScene);
+            PlayerPrefs.SetString("GameManager.LoadScene", sceneToLoad);
+            PlayerPrefs.SetString("GameManager.UnloadScene", unloadScene);
         }
     }
 }
