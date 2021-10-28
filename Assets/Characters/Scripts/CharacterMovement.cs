24e9b57878e9f7b83ffda577e6c795dabb47bfac
ENGR2881-hyde0070 <80891213+ENGR2881-hyde0070@users.noreply.github.com>
2021-10-28 12:11:20 +1030

    Character Movement
    I've finally finished and polished the character movement for cutscenes, personally I think it looks really smooth now.

---
+++ Assets/Characters/Scripts/CharacterMovement.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Transform[] waypoint;
+    int waypointIndex;
+    public GameObject idle;
+
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = waypoint[waypointIndex].position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+
+        if (waypoint[waypointIndex].position == transform.position)
+        {
+            nextWaypoint();
+        }
+    }
+
+    void nextWaypoint()
+    {
+        if (waypointIndex >= waypoint.Length - 1)
+        {
+            Destroy(gameObject);
+            idle.SetActive(true);
+        }
+
+        waypointIndex++;
+    }
+}
