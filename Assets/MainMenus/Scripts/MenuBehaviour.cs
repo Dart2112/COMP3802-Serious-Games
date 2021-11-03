c2178a4117e4a06d153d644668c046490638b6cd
ches0072 <ches0072@flinders.edu.au>
2021-11-04 09:14:55 +1030

    Remove greeting screen, re-assigned buttons

--- Assets/MainMenus/Scripts/MenuBehaviour.cs
+++ Assets/MainMenus/Scripts/MenuBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MenuBehaviour : MonoBehaviour
 {
-    public GameObject greetScreen;
+    //public GameObject greetScreen;
     public GameObject mainMenu;
     public GameObject playScene;
     public GameObject noteBook;
@@ -13,22 +13,31 @@ public class MenuBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        greetScreen.SetActive(true);
-        mainMenu.SetActive(false);
+       // greetScreen.SetActive(true);
+        mainMenu.SetActive(true);
         settings.SetActive(false);
     }
 
 
-    public void OpenMainMenu() {
+    /*public void OpenMainMenu() {
         greetScreen.SetActive(false);
         mainMenu.SetActive(true);
-    }
+    }*/
 
-    public void CloseMainMenu() {
+    /*public void CloseMainMenu() {
         mainMenu.SetActive(false);
         greetScreen.SetActive(true);
+    }*/
+
+    public void Quit() {
+#if UNITY_EDITOR
+        Debug.Log("hasQuit");
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
-    
+
     public void OpenSettingsMenu() {
         mainMenu.SetActive(false);
         settings.SetActive(true);
