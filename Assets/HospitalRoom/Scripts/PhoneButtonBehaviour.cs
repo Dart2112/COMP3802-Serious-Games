116e78eedb155cac34ec6ff4b27aa09d1647dde6
ches0072 <ches0072@flinders.edu.au>
2021-11-10 12:34:10 +1030

    Made some UI changes and script restructuring. Linked in physiotherapy dialogue scene to phone.

--- Assets/HospitalRoom/Scripts/PhoneButtonBehaviour.cs
+++ Assets/HospitalRoom/Scripts/PhoneButtonBehaviour.cs
@@ -4,10 +4,13 @@ namespace HospitalRoom.Scripts
 {
     public class PhoneButtonBehaviour : MonoBehaviour
     {
+        public void LoadPhysioEntrance() {
+            GameManager.Scripts.GameManager.LoadNewScene("Physiotherapist_Start", "Into_End");
+        }
 
         public void LoadPhysioTherapyPuzzle()
         {
-            GameManager.Scripts.GameManager.LoadNewScene("PhysioTherapyPuzzle", "Into_End");
+            GameManager.Scripts.GameManager.LoadNewScene("PhysioTherapyPuzzle", "Physiotherapist_Start");
         }
 
         public void LoadOTPuzzle()
--- Assets/HospitalRoom/Scripts/UIButtonBehaviour.cs
+++ Assets/HospitalRoom/Scripts/UIButtonBehaviour.cs
@@ -6,22 +6,31 @@ public class UIButtonBehaviour : MonoBehaviour
 {
     public GameObject phone;
     public GameObject noteBook;
+    private bool _allowOpen;
+    private string test;
 
     void Start() {
         noteBook.SetActive(false);
         phone.SetActive(false);
+        _allowOpen = false;
     }
 
     public void OpenPhone()
     {
+        if (_allowOpen) { 
         Debug.Log("openphone");
         phone.SetActive(true);
+        }        
     }
 
     public void OpenNotebook()
     {
+        if (_allowOpen)
+        {
         Debug.Log("opennotebook");
         noteBook.SetActive(true);
+        }
+        
     }
 
     public void CloseNotebook() {
@@ -32,5 +41,33 @@ public class UIButtonBehaviour : MonoBehaviour
     public void ClosePhone() {
         Debug.Log("closing phone");
         phone.SetActive(false);
+    }
+
+    public void AllowOpen() {
+        _allowOpen = true;
+    }
+
+    public void LoadPhysioEntrance()
+    {
+        test = GameManager.Scripts.GameManager.getCurrentScene();
+        Debug.Log("test is " + test);
+        GameManager.Scripts.GameManager.LoadNewScene("Physiotherapist_Start", test);
+    }
+
+    public void LoadPhysioTherapyPuzzle()
+    {
+        test = GameManager.Scripts.GameManager.getCurrentScene();
+        GameManager.Scripts.GameManager.LoadNewScene("PhysioTherapyPuzzle", test);
+    }
+
+/*    public void LoadOTEntrance()
+    {
+        GameManager.Scripts.GameManager.LoadNewScene("OT Puzzle", "Into_End");
+    }*/
+
+    public void LoadOTPuzzle()
+    {
+        test = GameManager.Scripts.GameManager.getCurrentScene();
+        GameManager.Scripts.GameManager.LoadNewScene("OT Puzzle", test);
     }
 }
