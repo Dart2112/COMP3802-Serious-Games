cc40745627eea0f5029a9fec30d26ca3213290a6
Benjamin <DNAblue2112@gmail.com>
2021-11-15 18:16:39 +1030

    Implemented Splash Screen

---
+++ Assets/MainMenus/Scripts/SplashScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MainMenus.Scripts
+{
+    public class SplashScreen : MonoBehaviour
+    {
+        public int timeToKeep = 120;
+        public int currentTime;
+
+        void FixedUpdate()
+        {
+            currentTime++;
+            if (currentTime > timeToKeep)
+            {
+                GameManager.Scripts.GameManager.LoadNewScene("MainMenus", "Splash Screen");
+            }
+        }
+    }
+}
